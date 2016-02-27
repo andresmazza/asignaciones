619f3b2baff38c4e5798eabc220d74a738ba8b3f
Andres Mazza <andres.mazza@gmail.com>
2016-02-27 20:04:57 -0300

    Add Entity

---
+++ sql/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `role`, `is_active`, `created_at`, `updated_at`) VALUES
+(1,	'andres',	'Andres',	'Mazza',	'andres.mazza@gmail.com',	'test',	'ROLE_ADMIN',	1,	'2016-02-27 19:44:58',	'2016-02-27 19:44:58'),
+(2,	'lolo',	'lolo',	'cat',	'lolo.cat@gmail.com',	'lolo',	'ROLE_USER',	1,	'2016-02-27 19:46:37',	'2016-02-27 19:46:37'),
+(3,	'lenovo',	'pc',	'lenovo',	'pc.lenovo@linux.com',	'lenovo',	'ROLE_USER',	1,	'2016-02-27 19:47:27',	'2016-02-27 19:47:27'),
+(4,	'clement',	'clemente',	'mazza',	'clement.mazza@mundoperruno.com',	'perro',	'ROLE_USER',	1,	'2016-02-27 19:48:24',	'2016-02-27 19:48:24'),
+(5,	'wimppy',	'wimppy',	'mazza',	'wimppy@mundoperruno.com',	'wimppy',	'ROLE_USER',	1,	'2016-02-27 19:49:04',	'2016-02-27 19:49:04');
