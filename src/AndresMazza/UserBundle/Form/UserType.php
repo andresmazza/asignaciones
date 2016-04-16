927e629ea867f4f88cffb15fc1fa0a156a89dbde
Andres Mazza <andres.mazza@gmail.com>
2016-04-16 18:57:41 -0300

    PRE_SET_DATA

--- src/AndresMazza/UserBundle/Form/UserType.php
+++ src/AndresMazza/UserBundle/Form/UserType.php
@@ -4,31 +4,41 @@ namespace AndresMazza\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvent;
 
 class UserType extends AbstractType {
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder
-            ->add('username')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email', 'email')
-            ->add('password', 'password')
-            ->add('role', 'choice', array (
-                    'choices' => array
-                    (
+        $builder->add('username')
+                ->add('firstName')
+                ->add('lastName')
+                ->add('email', 'email')
+                ->add('password', 'password')
+                ->add('role', 'choice', array(
+                    'choices' => array(
                         'ROLE_ADMIN' => 'Administrator',
-                        'ROLE_USER' => 'User'
+                        'ROLE_USER'  => 'User'
                     ),
-                    'placeholder' => 'Select a Role'))
-            ->add('isActive', 'checkbox')
-  //          ->add('createdAt')
+                    'placeholder' => 'Select a Role'
+                ))
+                ->add('isActive', 'checkbox')//          ->add('createdAt')
 //            ->add('updatedAt');
-        ->add('save','submit', array('label' => 'Save User'));
+                ->add('save', 'submit', array( 'label' => 'Save User' ));
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, array(
+                $this,
+                'onPreSetData'
+            ));
+    }
+
+
+    public function onPreSetData(FormEvent $event) {
+       // die("asdasdasdasdasdasd");
     }
 
     /**
